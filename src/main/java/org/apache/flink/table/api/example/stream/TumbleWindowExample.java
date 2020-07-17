c3918d8f44cf6f67cac5bf88c4b2049e8b3eb694
Jian He <hejian1639@sina.com>
2020-07-17 20:53:10 +0800

    tumble window example

---
+++ src/main/java/org/apache/flink/table/api/example/stream/TumbleWindowExample.java
@@ -0,0 +1,56 @@
+package org.apache.flink.table.api.example.stream;
+
+import lombok.val;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.java.StreamTableEnvironment;
+import org.apache.flink.types.Row;
+
+import java.sql.Timestamp;
+import java.util.Arrays;
+
+public class TumbleWindowExample {
+
+    public static void main(String[] args) throws Exception {
+
+        // 获取 environment
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        StreamTableEnvironment tEnv = StreamTableEnvironment.create(env);
+
+
+        // 初始数据
+        val log = env.fromCollection(Arrays.asList(
+                //时间 14:53:00
+                new Tuple2<>("xiao_ming", 300),
+                //时间 14:53:09
+                new Tuple2<>("zhang_san", 303),
+                //时间 14:53:12
+                new Tuple2<>("xiao_li", 204),
+                //时间 14:53:21
+                new Tuple2<>("li_si", 208)
+        ));
+
+
+        // 转换为 Table
+        Table logT = tEnv.fromDataStream(log, "name, v, t.proctime");
+
+        Table result = tEnv.sqlQuery("SELECT TUMBLE_START(t, INTERVAL '10' SECOND) AS window_start," +
+                "TUMBLE_END(t, INTERVAL '10' SECOND) AS window_end, SUM(v) FROM "
+                + logT + " GROUP BY TUMBLE(t, INTERVAL '10' SECOND)");
+
+        tEnv.toAppendStream(result, Row.class).print();
+
+        env.execute();
+    }
+
+
+}
